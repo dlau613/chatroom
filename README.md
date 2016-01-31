a2e44c31387234e8dc5c42d9b9c610ababb11ffd
Daniel Lau <daniel.s.lau@gmail.com>
2016-01-30 18:36:17 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+This chatroom was made by following this [tutorial](http://socket.io/get-started/chat/)
---
+++ server.js
@@ -0,0 +1,40 @@
+//load modules
+// var express = require("express");
+// var app = express();
+// var http = require("http").createServer(app);
+// var port = 8001;
+
+// app.get("/", function(req,res) {
+// 	res.send("it works!");
+// });
+
+// app.listen(port);
+// console.log("listening on port" + port);
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  // res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
+	});
+	socket.on('chat message', function(msg){
+    	console.log('message: ' + msg);
+    	io.emit('chat message', msg);
+ 	});
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+
