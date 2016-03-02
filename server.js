f08bfbc2563d41051373b25f24b90d684cdae643
Daniel Lau <daniel.s.lau@gmail.com>
2016-03-01 23:15:03 -0800

    idk

--- server.js
+++ server.js
@@ -16,12 +16,9 @@ var app = express();
 var http = require('http').createServer(app);
 console.log("aaaaaaaaaa");
 var io = require('socket.io')(http);
-
+console.log(io);
 // app.listen(port, '0.0.0.0');
 // http.listen(3000);
-http.listen(port, function(){
-  console.log('listening on *:3000');
-});
 // app.listen(port, '0.0.0.0', function(err) {
 //   console.log("Started listening on %s", app.url);
 // });
@@ -83,6 +80,9 @@ io.on('connection', function(socket){
 });
 
 
+http.listen(port, function(){
+  console.log('listening on *:3000');
+});
 
 // http.listen(3000, function(){
 //   console.log('listening on *:3000');
